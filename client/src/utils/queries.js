225b23956d5a1d018c367db9f62f92979ca92bd8
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-22 12:48:27 -0500

    finish queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,19 @@
 import { gql} from '@apollo/client';
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
-      _id
-      tech1
-      tech2
+export const GET_ME = gql`{
+me{
+_id
+username
+email
+bookCount
+savedBook{
+  # _id
+  bookId
+  authors
+  description
+  title
+  image
+  link
     }
   }
-`;
-
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
   }
 `;
