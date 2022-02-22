cd066babab504f1b97d936fdc971be98c952604d
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-22 10:31:58 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const {ApolloServer} = require('apollo-server-express');
-const { authMiddleware } = requie('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers} = require('./schemas')
 
